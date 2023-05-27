4615dcfd64fbc1fa83c7200b2def7bed0fccccff
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-05-27 14:47:15 +0930

    chat function completely done, need to design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import Spinner from 'react-bootstrap/Spinner';
+import React, { useState,useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+import { useMutation } from '@apollo/client';
 import Auth from '../../src/utils/auth';
 import Container from 'react-bootstrap/Container';
+import { DELETE_ALL_DATA } from '../utils/mutations';
 
 
 
@@ -19,13 +19,32 @@ const styles = {
 
 
 
+
 const Home = () => {
+
 
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
 
+  const [deleteAllData] = useMutation(DELETE_ALL_DATA);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        
+        await deleteAllData();
+        
+        console.log('Data deleted successfully.');
+        
+      } catch (error) {
+        console.error('Error deleting data:', error);
+      }
+    };
+    fetchData();
+
+  }, []);
   
   return (
     <main>
